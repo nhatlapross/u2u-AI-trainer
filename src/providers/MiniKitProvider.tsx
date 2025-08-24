a4fa08f99fff11c091febfab8872093db4a23b7e
Alvin Ichi <nhatlapross@gmail.com>
2025-08-24 23:44:12 +0700

    update

--- src/providers/MiniKitProvider.tsx
+++ src/providers/MiniKitProvider.tsx
@@ -1,12 +1,24 @@
-'use client';
-import { MiniKitProvider } from '@coinbase/onchainkit/minikit';
-import { ReactNode } from 'react';
-import { base } from 'wagmi/chains';
+"use client";
 
-export function MiniKitContextProvider({ children }: { children: ReactNode }) {
+import { type ReactNode } from "react";
+import { base } from "wagmi/chains";
+import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
+
+export function Providers(props: { children: ReactNode }) {
   return (
-    <MiniKitProvider apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY} chain={base}>
-      {children}
+    <MiniKitProvider
+      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+      chain={base}
+      config={{
+        appearance: {
+          mode: "auto",
+          theme: "mini-app-theme",
+          name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+          logo: process.env.NEXT_PUBLIC_ICON_URL,
+        },
+      }}
+    >
+      {props.children}
     </MiniKitProvider>
   );
 }
