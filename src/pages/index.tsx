de323b969554b34e758d0ad2990857d3cf2ec6e8
nhatlapross <nhatlapross@gmail.com>
2025-09-06 13:39:37 +0700

    fix setup onchainkit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useMiniKit } from '@coinbase/onchainkit/minikit';
 import { NextPage } from 'next';
 import BlockchainFitnessLanding from "@/components/LandingPage/Landing";
 
 const Home: NextPage = () => {
+    const { setFrameReady, isFrameReady } = useMiniKit();
+
+    useEffect(() => {
+        if (!isFrameReady) setFrameReady();
+    }, [isFrameReady, setFrameReady]);
     return (
         <div className="w-full">
             <BlockchainFitnessLanding />
