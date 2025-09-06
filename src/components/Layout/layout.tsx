e390ff94a5d4e06c691eb848bd4d98b039877e96
nhatlapross <nhatlapross@gmail.com>
2025-09-06 11:06:11 +0700

    update image

--- src/components/Layout/layout.tsx
+++ src/components/Layout/layout.tsx
@@ -2,13 +2,18 @@
 import { ReactNode, useEffect } from 'react';
 import Navbar from '../Header/index';
 import Footer from '../Footer/index';
+import { useMiniKit } from '@coinbase/onchainkit/minikit';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
-  
+  const { setFrameReady, isFrameReady } = useMiniKit();
+
+  useEffect(() => {
+    if (!isFrameReady) setFrameReady();
+  }, [isFrameReady, setFrameReady]);
   return (
     <>
       <Navbar />
